e4a3073f3d3d2b9588a4884226316281f72bb453
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-02-29 01:47:13 +0100

    Add files via upload

---
+++ inst/extdata/examples/ex_mod_plots_tracking.R
@@ -0,0 +1,20 @@
+library(omXplore)
+
+options(shiny.fullstacktrace = TRUE)
+
+ui <- fluidPage(
+    mod_plots_tracking_ui("tracker")
+)
+
+
+
+server <- function(input, output, session) {
+    data(vData_ft)
+    obj <- vData_ft[[1L]]
+
+    mod_plots_tracking_server("tracker",
+                              obj = reactive({obj}))
+}
+
+if (interactive())
+    shinyApp(ui, server)
