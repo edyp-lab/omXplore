01287bb66a43c58a7040b79870239a70ae4d8c29
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2024-03-06 05:21:19 +0100

    Update doc-data.R

--- R/doc-data.R
+++ R/doc-data.R
@@ -80,7 +80,7 @@
 
 ##' Feature example data
 ##'
-##' `vdata` is a small test `MultiAssayExperiment` object for testing and
+##' `vdata` is a small object for testing and
 ##' demonstration.  `vdata_na`
 ##' is a tiny test set that contains missing values used to
 ##' demonstrate and test the impact of missing values on data
@@ -96,37 +96,46 @@
 ##'
 "vdata"
 
-# ##' Example `QFeatures` object after processing
-# ##' 
-# ##' `feat3` is a small `QFeatures` object that contains 7 assays: 
-# ##' `psms1`, `psms2`, `psmsall`, `peptides`, `proteins`, 
-# ##' `normpeptides`, `normproteins`. The dataset contains example data
-# ##' that could be obtained after running a simple processing pipeline.
-# ##' You can use it to get your hands on manipulating `AssayLinks` 
-# ##' since all 3 general cases are present:
-# ##' * One parent to one child `AssayLink`: the relationship can either be 
-# ##'   one row to one row (e.g. "peptides" to "normpeptides") or 
-# ##'   multiple rows to one row (e.g. "peptides" to "proteins").
-# ##' * One parent to multiple children `AssayLink`: for instance "peptides"
-# ##'   to "normpeptides" and "proteins".
-# ##' * Multiple parents to one child `AssayLink`: links the rows between 
-# ##'   multiple assays to a single assays where some rows in different 
-# ##'   parent assays may point to the same row in the child assay. E.g.
-# ##'   "psms1" and "psms2" to "psmsall"
-# ##'
-# ##' @source
-# ##'
-# ##' `feat3` was built from `feat1`. The source code is available in
-# ##' [`inst/scripts/test_data.R`](https://github.com/rformassspectrometry/QFeatures/blob/master/inst/scripts/test_data.R)
-# ##' 
-# ##' @seealso
-# ##'
-# ##' See `?feat1` for other example/test data sets. 
-# ##' 
-# ##' @examples 
-# ##' 
-# ##' data("feat3")
-# ##' plot(feat3)
-# ##' 
-# ##' @md
-# "feat3"
+
+##' Feature example data
+##'
+##' `sub_R25_prot` is a protein subset of the dataset 'Exp1_R25_prot' in the 
+##' package 'DAPARdata'. 
+##'
+##' @source
+##'
+##' `sub_R25_prot` was built from the source code available in
+##' [`inst/scripts/build_datasets.R`](https://github.com/prostarproteomics/omXplore/blob/main/inst/scripts/build_datasets.R)
+##'  
+##' The `DAPARdata` package: \url{https://github.com/prostarproteomics/DAPARdata}
+##'
+##' @aliases sub_R25_pept
+##'
+##' @examples
+##' 
+##' data(sub_R25_prot)
+##' view_dataset(sub_R25_prot)
+##' 
+"sub_R25_prot"
+
+##' Feature example data
+##'
+##' `sub_R25_pept` is a protein subset of the dataset 'sub_R25_pept' in the 
+##' package 'DAPARdata'. 
+##'
+##' @source
+##'
+##' `sub_R25_pept` was built from the source code available in
+##' [`inst/scripts/build_datasets.R`](https://github.com/prostarproteomics/omXplore/blob/main/inst/scripts/build_datasets.R)
+##'  
+##' The `DAPARdata` package: \url{https://github.com/prostarproteomics/DAPARdata}
+##'
+##' @aliases sub_R25_prot
+##' 
+##' @examples
+##' 
+##' data(sub_R25_pept)
+##' view_dataset(sub_R25_pept)
+##' 
+##'
+"sub_R25_pept"
